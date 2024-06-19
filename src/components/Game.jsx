db721cfd4a22092b876b550a0787dafcf19aebfa
iambasilp <iambasilp@gmail.com>
2024-06-19 15:40:53 +0530

    updated

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,21 @@
-import Board from "./Board"
+import Board from "./Board";
 
 const Game = () => {
   return (
     <div>
-      <Board  />
+      <div className="game-content">
+        <h1 className="game">Tic Tac Toe</h1>
+        <p>
+          Tic-tac-toe, noughts and crosses, or Xs and Os is a paper-and-pencil
+          game for two players who take turns marking the spaces in a
+          three-by-three grid with X or O. The player who succeeds in placing
+          three of their marks in a horizontal, vertical, or diagonal row is the
+          winner
+        </p>
+      </div>
+      <Board />
     </div>
-  )
-}
+  );
+};
 
-export default Game
+export default Game;
