a6bc1152d95adf380db1d5db36962d01803df0ec
iambasilp <iambasilp@gmail.com>
2024-06-19 10:49:25 +0530

    basic style added

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,11 +1,26 @@
-
-
 const TicTacToe = () => {
   return (
-    <div>
-      
+    <div className="tic-wrapper">
+      <div className="tic-content">
+        <h1 className="tic-heading">Tic Tac Toe</h1>
+        <p className="tic-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          deleniti veniam, est corporis ducimus 
+        </p>
+      </div>
+      <div className="tic-box">
+        <div className="box box-1"></div>
+        <div className="box box-2"></div>
+        <div className="box box-3"></div>
+        <div className="box box-4"></div>
+        <div className="box box-5"></div>
+        <div className="box box-6"></div>
+        <div className="box box-7"></div>
+        <div className="box box-8"></div>
+        <div className="box box-9"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TicTacToe
+export default TicTacToe;
