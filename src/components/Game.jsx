acd4af1830ba11e99df45e46d063f8070dce5d3f
iambasilp <iambasilp@gmail.com>
2024-06-20 12:00:08 +0530

    updated

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,6 +5,7 @@ import { calculateWinner } from "./calculateWinner";
 // overite we can not do when click
 
 const Game = () => {
+
   const [board, setBoard] = useState([
     null,
     null,
@@ -18,6 +19,16 @@ const Game = () => {
   ]);
   const [flag, setflag] = useState(true);
   let winner = calculateWinner(board);
+  const player1 = ["🍎","🚗","🌹"];
+  const player2 = ["🍌","🐝","🐥"]
+
+  function handlePlayer1(e){
+    setPlayer1()
+  }
+  function handlePlayer2(e){
+    console.log(e.target.value)
+  }
+
   function handleClick(index) {
     let boardcopy = [...board];
     if (boardcopy[index] || winner) 
@@ -30,6 +41,7 @@ const Game = () => {
   }
   let isDraw = board.every((item)=> item!=null)
   console.log(isDraw);
+
   return (
     <div className="game">
       <div className="game-content">
@@ -41,6 +53,19 @@ const Game = () => {
           three of their marks in a horizontal, vertical, or diagonal row is the
           winner
         </p>
+        <div className="select-choose">
+        <select name="" id="" className="select-chooseFirst" onChange={handlePlayer1}>
+         {player1.map((item,index)=>{
+          return <option key={index}>{item}</option>
+         })}
+        </select>
+        <select name="" id="" className="select-chooseSecond" onChange={handlePlayer2}>
+        {player2.map((item,index)=>{
+          return <option key={index}>{item}</option>
+         })}
+        </select>
+        </div>
+ 
       </div>
       <Board squres={board} onClick={handleClick} />
       <div className="info">
